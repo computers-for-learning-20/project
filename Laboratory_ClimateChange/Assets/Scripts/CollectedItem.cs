db7c6905ce637ca7141d458340c9c36e025bf0a6
Jesica Ramirez <jramireztoscano@uchicago.edu>
2020-11-30 18:20:19 -0600

    merge changes

---
+++ Laboratory_ClimateChange/Assets/Scripts/CollectedItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectedItem : MonoBehaviour
+{
+
+    //public ParticleSystem GoalParticles;
+    //public ParticleSystem GoalCollision;
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager")
+            .GetComponent<GameBehavior>();
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //Put collision code here
+        if (collision.gameObject.name == "Spy")
+        {
+            // triggers the burst of white
+            // particles and stops the small yellow ones
+            //GoalCollision.Play();
+            //Destroy(GoalParticles);
+
+            //Update goals colected
+            //gameManager.Goals += 1;
+
+            // removed the collected goal
+            Destroy(this.transform.gameObject);
+        }
+    }
+}
--- Laboratory_ClimateChange/Assets/Scripts/SpyBehaviour.cs
+++ Laboratory_ClimateChange/Assets/Scripts/SpyBehaviour.cs
@@ -10,30 +10,27 @@ public class SpyBehaviour : MonoBehaviour
     public float rot = 0f;
     public float gravity = 8;
     public float jumpVelocity = 5f;
-    public float groundDistance = 0.3f;
-    public LayerMask whatIsGround;
     Vector3 moveDir = Vector3.zero;
     CharacterController controller;
     Animator anim;
+    public GameBehavior gameManager;
+    //public ParticleSystem FireCollision
     private Rigidbody _rb;
-    private float fbInput;
-    private float lrInput;
+
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        gameManager = GameObject.Find("GameManager")
+            .GetComponent<GameBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        fbInput = Input.GetAxis("Vertical")*moveSpeed;
-        lrInput = Input.GetAxis("Horizontal")*rotateSpeed;
-
-        Move();
-        
+        Move();   
         
     }
 
@@ -87,6 +84,35 @@ public class SpyBehaviour : MonoBehaviour
         controller.Move(moveDir*Time.deltaTime);
     }
 
+    void OnCollisionEnter(Collision collision){
+        
+        //Decrease Spy's health if it collides with fire
+        if (collision.gameObject.name == "battery" | collision.gameObject.name == "battery_small")
+        {
+            gameManager.Batteries += 1;
+        }
+
+        else if (collision.gameObject.name == "solar_panel"){
+            gameManager.SolarPanels +=1;
+        }
     
-    
+    }
+
+    void OnParticleCollision(GameObject other){
+        
+        if(other.name == "Fire")
+        {
+               //FireCollision.Play();
+               if(gameManager.HealthSpy <= 1)
+            {
+                gameManager.HealthSpy = 0;
+                Debug.Log("Ouch! You've lost!");
+            }
+            else
+            {
+                gameManager.HealthSpy -= 1;
+                Debug.Log("Ouch! Spy's Health decreased!");
+            }
+        }
+    }
 }
